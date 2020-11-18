b573e4a3b84738a6674683af87daf1363d088f74
Neale Johnston <neale@team-johnston.com>
2020-11-18 20:47:24 +0000

    Test passing

--- upper_caser.js
+++ upper_caser.js
@@ -1,8 +1,10 @@
 const UpperCaser = function (words) {
+  this.words = words;
 }
 
 UpperCaser.prototype.toUpperCase = function () {
-
+  const upperCase = this.words.map(word => word.toUpperCase())
+  return upperCase
 }
 
 module.exports = UpperCaser;
